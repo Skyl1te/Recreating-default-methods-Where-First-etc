9a28aed3b916bed7daaf707e3eea526dcaf71b33
Chuhuivec Andrii <chuchu.andreimax@gmail.com>
2024-06-26 13:28:50 +0300

    Add project files.

---
+++ Yield-Return/EnumerableExtensions.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Yield_Return;
+
+// Класс с методами расширения
+public static class EnumerableExtensions
+{
+    public static void Print<T>(this IEnumerable<T> collection)
+    {
+        foreach (T item in collection)
+        {
+            Console.Write($"{item} ");
+        }
+        Console.WriteLine();
+    }
+
+    public static int MyCount<T>(this IEnumerable<T> collection)
+    {
+        int count = 0;
+        foreach (T item in collection)
+        {
+            count++;
+        }
+        return count;
+    }
+
+    public static IEnumerable<T> MyWhere<T>(this IEnumerable<T> collection, Func<T, bool> func)
+    {
+        foreach (T item in collection)
+        {
+            if (func.Invoke(item))
+            {
+                yield return item;
+            }
+        }
+    }
+
+    public static T MyFirst<T>(this IEnumerable<T> collection, Func<T, bool> func)
+    {
+        foreach (T item in collection)
+        {
+            if (func.Invoke(item))
+            {
+                return item;
+            }
+        }
+
+        throw new InvalidOperationException("Sequence contains no matching element.");
+    }
+
+    public static bool MyAny<T>(this IEnumerable<T> collection, Func<T, bool> func)
+    {
+        foreach (T item in collection)
+        {
+            if (func.Invoke(item))
+            {
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    public static bool MyAll<T>(this IEnumerable<T> collection, Func<T, bool> func)
+    {
+        foreach (T item in collection)
+        {
+            if (!func.Invoke(item))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static int MyCountOf<T>(this IEnumerable<T> collection, Func<T, bool> func)
+    {
+        int count = 0;
+        foreach (T item in collection) 
+        {
+            if (func.Invoke(item))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static T MyLast<T>(this IEnumerable<T> collection, Func<T, bool> func)
+    {
+        T lastItem = default;
+        bool isFound = default;
+        foreach (T item in collection)
+        {
+            if (func.Invoke(item))
+            {
+                lastItem = item;
+                isFound = true;
+            }
+        }
+        if (!isFound)
+        {
+            throw new InvalidOperationException("Sequence contains no matching element.");
+        }
+        else
+        {
+            return lastItem;
+        }
+    }
+
+}
---
+++ Yield-Return/Program.cs
@@ -0,0 +1,29 @@
+﻿// IEnumerable (GetEnumerator(), inherit)
+// yield return
+// Extension methods
+// Recreating default methods Where, FirstOrDefault, ...
+
+using Yield_Return;
+
+string[] words = { "adsad", "sdasdsa", "qqqqe" };
+Console.WriteLine(words.MyCount());
+
+Console.WriteLine("----------------");
+
+int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+foreach (int num in nums.MyWhere(i => i % 2 == 0))
+{
+    Console.WriteLine(num);
+}
+
+Console.WriteLine("----------------");
+
+Console.WriteLine(nums.MyFirst(i => i % 2 == 0));
+
+Console.WriteLine("----------------");
+
+Console.WriteLine(nums.MyCountOf(i => i % 2 == 0));
+
+Console.WriteLine("----------------");
+
+Console.WriteLine(nums.MyLast(i => i % 2 == 0));
